14357b63b5aeb9130d65aa63f13064163443405a
jonathan Morton <jmorton4193@gmail.com>
2017-10-10 03:50:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeliLine=[]
+function takeANumber (katzDeliLine, customName){
+  katzDeliLine.push(customName)
+return `Welcome, ${customName}. You are number ${katzDeliLine.length} in line.`
+
+}
+
+function nowServing (katzDeliLine){
+if (katzDeliLine.length>=1){
+return `Currently serving ${katzDeliLine.shift()}.`
+}
+else{
+return "There is nobody waiting to be served!"}
+}
+
+function currentLine(){}
