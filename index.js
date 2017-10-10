a698dfc7da12962fe0aa612e4f04285089c0de6a
jonathan Morton <jmorton4193@gmail.com>
2017-10-10 04:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ return "There is nobody waiting to be served!"}
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length==0){return"The line is currently empty."}
-  else { for (let i=0; i<katzDeliLine.length; i++)
-    var k= []
-    k.push(katzDeliLine[i])
-    }
-    return `The line is Currently: ${k},`
+  else { var namesAndNumber=[]
+    for (let i=0; i<katzDeliLine.length; i++)
+    var k= `${i+1}\. ${katzDeliLine[i]}`
+    namesAndNumber.push(k)
+        }
+    return `Current Line: ${namesAndNumber}`
   }
